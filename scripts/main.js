a1f4454374285bff8d39264154819a1c285ffab6
Justin Thornton <jathom15@gmail.com>
2021-12-30 13:10:49 -0600

    Class refactor

--- scripts/main.js
+++ scripts/main.js
@@ -7,164 +7,167 @@ const newBookSection = document.getElementById("form-section");
 const newBookSubmitButton = document.getElementById("subBtn");
 const newBookCloseButton = document.getElementById("clBtn");
 
-let myLibrary = [
-	new book("Harry Potter and the Chamber of Secrets", "JK Rowling", 251),
-	new book("Harry Potter and the Sorcerer's Stone", "JK Rowling", 223),
-	new book("Harry Potter and the Prisoner of Azkaban", "JK Rowling", 317),
-	new book("Harry Potter and the Goblet of Fire", "JK Rowling", 636),
-	new book("Harry Potter and the Order of the Phoenix", "JK Rowling", 766),
-	new book("The Outsiders", "S.E. Hinton", 192),
-];
+class book {
 
-function book(title, author, pages) {
-	this.title = title;
-	this.author = author;
-	this.pages = pages;
-	this.read = false;
-}
+	static myLibrary = [
+		new book("Harry Potter and the Chamber of Secrets", "JK Rowling", 251),
+		new book("Harry Potter and the Sorcerer's Stone", "JK Rowling", 223),
+		new book("Harry Potter and the Prisoner of Azkaban", "JK Rowling", 317),
+		new book("Harry Potter and the Goblet of Fire", "JK Rowling", 636),
+		new book("Harry Potter and the Order of the Phoenix", "JK Rowling", 766),
+		new book("The Outsiders", "S.E. Hinton", 192),
+	];
 
-book.prototype.addBooks = function () {
-	myLibrary.push(this);
-	newBookSection.style.display = "none";
-	return;
-};
-
-book.prototype.removeBook = function (dataAttribute, el, childEl) {
-	const index = parseInt(dataAttribute);
-	if (index > -1) {
-		myLibrary.splice(index, 1);
-		el.removeChild(childEl);
+	static bookCount(obj) {
+		return obj.length;
 	}
-	totalBooks.textContent = bookCount(myLibrary);
-	readBooks.textContent = readBookCount(myLibrary);
-	dataAttributeUpdate();
-}
 
-book.prototype.display = function (index) {
-	const bookDetails = document.createElement("div");
-	bookDetails.classList.add("book");
-	bookDetails.setAttribute("data-value", index);
+	static readBookCount(obj) {
+		let count = 0;
+		obj.forEach((index) => {
+			if (index.read) {
+				count += 1;
+			}
+		});
+		return count;
+	}
 
-	const bold = document.createElement("strong");
-	bold.textContent = "Title:";
-	bold.classList.add("bookFontSizeStrong");
-
-	const bookTitle = document.createElement("div");
-	bookTitle.textContent = this.title;
-	bookTitle.classList.add("bookFontSize");
-
-	const bookAuthor = document.createElement("div");
-	bookAuthor.textContent = `By: ${this.author}`;
-	bookAuthor.classList.add("bookFontSize");
-
-	const bookPages = document.createElement("div");
-	bookPages.textContent = `${this.pages} Pages`;
-	bookPages.classList.add("bookFontSize");
-
-	const brk = document.createElement("br");
-
-	const bookRead = document.createElement("button");
-	const check = document.createElement("div");
-	check.classList.add("checkmark");
-	check.classList.add("draw");
-
-	bookRead.setAttribute("id", "read-button");
-	bookRead.addEventListener("mouseover", () => {
-		bookRead.style.backgroundColor = "#5cb85c";
-	});
-	bookRead.addEventListener("mouseleave", () => {
-		bookRead.style.backgroundColor = "#7d2ae8";
-	});
-
-	bookRead.onclick = () => {
-		this.read = !this.read;
-		readBooks.textContent = readBookCount(myLibrary);
-
-		//animated checkmark for read books
-		if (this.read) {
-			check.style.display = "block";
-			bookRead.textContent = "Mark as unread";
-			bookRead.addEventListener("mouseover", () => {
-				bookRead.style.backgroundColor = "#d11a2a";
-			});
-			bookRead.addEventListener("mouseleave", () => {
-				bookRead.style.backgroundColor = "#7d2ae8";
-			});
-		} else {
-			check.style.display = "none";
-			bookRead.textContent = "Mark as read";
-			bookRead.addEventListener("mouseover", () => {
-				bookRead.style.backgroundColor = "#5cb85c";
-			});
-			bookRead.addEventListener("mouseleave", () => {
-				bookRead.style.backgroundColor = "#7d2ae8";
-			});
-		}
-	};
-	bookRead.textContent = "Mark as read";
-
-	// create delete button symbol
-	const delBookIcon = document.createElement("button");
-	delBookIcon.textContent = ":";
-	delBookIcon.classList.add("remove-book");
-
-	//create hidden delete button
-	const delBookButton = document.createElement("button");
-	delBookButton.textContent = "Remove Book";
-	delBookButton.setAttribute("id", "bookDeleteButton");
-	delBookButton.style.display = "none";
-
-	delBookIcon.addEventListener("click", () => {
-		delBookButton.style.display = "block";
-	});
-
-	delBookButton.addEventListener("click", () => {
-		this.removeBook(bookDetails.dataset.value, container, bookDetails);
-		delBookButton.style.display = "none";
-	});
-
-	//close delete button if click is not on the button
-	document.addEventListener("click", (e) => {
-		if (!delBookIcon.contains(e.target)) {
-			delBookButton.style.display = "none";
-		}
-	});
-
-	container.appendChild(bookDetails);
-	bookDetails.appendChild(bold);
-	bookDetails.appendChild(bookTitle);
-	bookDetails.appendChild(bookAuthor);
-	bookDetails.appendChild(bookPages);
-	bookDetails.appendChild(brk);
-	bookDetails.appendChild(bookRead);
-	bookDetails.appendChild(delBookIcon);
-	bookDetails.appendChild(delBookButton);
-	bookDetails.appendChild(check);
-};
-
-book.prototype.edit = function () {};
-
-
-function bookCount(obj) {
-	return obj.length;
-}
-
-function readBookCount(obj) {
-	let count = 0;
-	obj.forEach((index) => {
-		if (index.read) {
+	static dataAttributeUpdate() {
+		let count = 0;
+		Array.from(dataAtts).forEach((d) => {
+			d.dataset.value = count;
 			count += 1;
-		}
-	});
-	return count;
-}
+		});
+	}
 
-function dataAttributeUpdate() {
-	let count = 0;
-	Array.from(dataAtts).forEach((d) => {
-		d.dataset.value = count;
-		count += 1;
-	});
+	constructor(title, author, pages) {
+		this.title = title;
+		this.author = author;
+		this.pages = pages;
+		this.read = false;
+	}
+
+	addBooks = function () {
+		book.myLibrary.push(this);
+		newBookSection.style.display = "none";
+		return;
+	};
+
+	removeBook = function (dataAttribute, el, childEl) {
+		const index = parseInt(dataAttribute);
+		if (index > -1) {
+			book.myLibrary.splice(index, 1);
+			el.removeChild(childEl);
+		}
+		totalBooks.textContent = book.bookCount(book.myLibrary);
+		readBooks.textContent = book.readBookCount(book.myLibrary);
+		book.dataAttributeUpdate();
+	};
+
+	display = function (index) {
+		const bookDetails = document.createElement("div");
+		bookDetails.classList.add("book");
+		bookDetails.setAttribute("data-value", index);
+
+		const bold = document.createElement("strong");
+		bold.textContent = "Title:";
+		bold.classList.add("bookFontSizeStrong");
+
+		const bookTitle = document.createElement("div");
+		bookTitle.textContent = this.title;
+		bookTitle.classList.add("bookFontSize");
+
+		const bookAuthor = document.createElement("div");
+		bookAuthor.textContent = `By: ${this.author}`;
+		bookAuthor.classList.add("bookFontSize");
+
+		const bookPages = document.createElement("div");
+		bookPages.textContent = `${this.pages} Pages`;
+		bookPages.classList.add("bookFontSize");
+
+		const brk = document.createElement("br");
+
+		const bookRead = document.createElement("button");
+		const check = document.createElement("div");
+		check.classList.add("checkmark");
+		check.classList.add("draw");
+
+		bookRead.setAttribute("id", "read-button");
+		bookRead.addEventListener("mouseover", () => {
+			bookRead.style.backgroundColor = "#5cb85c";
+		});
+		bookRead.addEventListener("mouseleave", () => {
+			bookRead.style.backgroundColor = "#7d2ae8";
+		});
+
+		bookRead.onclick = () => {
+			this.read = !this.read;
+			readBooks.textContent = book.readBookCount(book.myLibrary);
+
+			//animated checkmark for read books
+			if (this.read) {
+				check.style.display = "block";
+				bookRead.textContent = "Mark as unread";
+				bookRead.addEventListener("mouseover", () => {
+					bookRead.style.backgroundColor = "#d11a2a";
+				});
+				bookRead.addEventListener("mouseleave", () => {
+					bookRead.style.backgroundColor = "#7d2ae8";
+				});
+			} else {
+				check.style.display = "none";
+				bookRead.textContent = "Mark as read";
+				bookRead.addEventListener("mouseover", () => {
+					bookRead.style.backgroundColor = "#5cb85c";
+				});
+				bookRead.addEventListener("mouseleave", () => {
+					bookRead.style.backgroundColor = "#7d2ae8";
+				});
+			}
+		};
+		bookRead.textContent = "Mark as read";
+
+		// create delete button symbol
+		const delBookIcon = document.createElement("button");
+		delBookIcon.textContent = ":";
+		delBookIcon.classList.add("remove-book");
+
+		//create hidden delete button
+		const delBookButton = document.createElement("button");
+		delBookButton.textContent = "Remove Book";
+		delBookButton.setAttribute("id", "bookDeleteButton");
+		delBookButton.style.display = "none";
+
+		delBookIcon.addEventListener("click", () => {
+			delBookButton.style.display = "block";
+		});
+
+		delBookButton.addEventListener("click", () => {
+			this.removeBook(bookDetails.dataset.value, container, bookDetails);
+			delBookButton.style.display = "none";
+		});
+
+		//close delete button if click is not on the button
+		document.addEventListener("click", (e) => {
+			if (!delBookIcon.contains(e.target)) {
+				delBookButton.style.display = "none";
+			}
+		});
+
+		container.appendChild(bookDetails);
+		bookDetails.appendChild(bold);
+		bookDetails.appendChild(bookTitle);
+		bookDetails.appendChild(bookAuthor);
+		bookDetails.appendChild(bookPages);
+		bookDetails.appendChild(brk);
+		bookDetails.appendChild(bookRead);
+		bookDetails.appendChild(delBookIcon);
+		bookDetails.appendChild(delBookButton);
+		bookDetails.appendChild(check);
+	};
+
+
+	edit = function () {};
 }
 
 function openForm() {
@@ -174,6 +177,7 @@ function openForm() {
 function closeForm() {
 	newBookSection.style.display = "none";
 }
+
 
 //close "add new book" window if click is not on the form window
 newBookSection.addEventListener("click", (e) => {
@@ -189,9 +193,9 @@ newBookForm.addEventListener("submit", function () {
 	const pagesValue = newBookForm.elements["pages"].value;
 	const newBook = new book(titleValue, authorValue, pagesValue);
 	newBook.addBooks();
-	newBook.display(myLibrary.length - 1);
+	newBook.display(book.myLibrary.length - 1);
 	newBookForm.reset();
-	totalBooks.textContent = bookCount(myLibrary);
+	totalBooks.textContent = book.bookCount(book.myLibrary);
 });
 
 newBookCloseButton.addEventListener("click", () => {
@@ -200,9 +204,9 @@ newBookCloseButton.addEventListener("click", () => {
 });
 
 //initial library display
-myLibrary.forEach((book, index) => {
+book.myLibrary.forEach((book, index) => {
 	book.display(index);
 });
 
-totalBooks.textContent = bookCount(myLibrary);
-readBooks.textContent = readBookCount(myLibrary);
+totalBooks.textContent = book.bookCount(book.myLibrary);
+readBooks.textContent = book.readBookCount(book.myLibrary);
